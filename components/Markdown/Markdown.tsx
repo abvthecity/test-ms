f706bbeea33739ecb5334d14f82951d077d99f76
Andrew Jiang <andrewjiang@hey.com>
2025-02-10 01:00:34 -0500

    center-aligned

--- components/Markdown/Markdown.tsx
+++ components/Markdown/Markdown.tsx
@@ -1,10 +1,12 @@
 import ReactMarkdown from 'react-markdown'
 
+import remarkGfm from 'remark-gfm'
+
 export default function Markdown({ children }: { children: string }) {
   if (typeof children !== 'string') return null
   return (
-    <article className="prose">
-      <ReactMarkdown>{children}</ReactMarkdown>
+    <article className="prose mx-auto">
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{children}</ReactMarkdown>
     </article>
   )
 }
